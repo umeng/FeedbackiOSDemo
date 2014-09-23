99ffbaa9b73068e89e02b7aefeb3c0742821a4e5
amoblin@mbp <amoblin@gmail.com>
2014-09-23 13:30:05 +0800

    add empty Swift Demo

---
+++ SwiftDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftDemo
+//
+//  Created by amoblin on 14/9/23.
+//  Copyright (c) 2014年 umeng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
