73d947a01af2eb38f07956b3b8d3174add5314ff
amoblin@mbp <amoblin@gmail.com>
2014-09-23 13:34:46 +0800

    Feedback Demo with Swift

---
+++ SwiftDemo/SwiftDemo-Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  SwiftDemo-Bridging-Header.h
+//  UMFeedback
+//
+//  Created by amoblin on 14/9/17.
+//
+//
+
+#import "UMFeedback.h"
+
+#define UMENG_APPKEY @"4eeb0c7b527015643b000003"
--- SwiftDemo/ViewController.swift
+++ SwiftDemo/ViewController.swift
@@ -13,6 +13,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var btn = UIButton(frame: CGRectMake(100, 200, 120, 20));
+        btn.setTitleColor(UIColor(red: 0, green: 122.0/255, blue: 1.0, alpha: 1.0), forState: UIControlState.Normal);
+        btn.setTitle("Feedback", forState:UIControlState.Normal);
+        btn.addTarget(self, action: "webFeedback:", forControlEvents: UIControlEvents.TouchUpInside);
+        self.view.addSubview(btn);
+    }
+
+    func webFeedback(sender: UIButton!) {
+        UMFeedback.showFeedback(self, withAppkey: UMENG_APPKEY);
     }
 
     override func didReceiveMemoryWarning() {
